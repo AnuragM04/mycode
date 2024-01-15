ffca8cdbac19f10b0e6603de87b3b738fd917fb4
Praveen Duddu <praveen.duddu@bbc.co.uk>
2024-01-15 19:29:25 +0000

    Refactor

--- java/Projects/PMS/src/Main.java
+++ java/Projects/PMS/src/Main.java
@@ -1,11 +1,7 @@
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
         PasswordManager pm = new PasswordManager();
-        //PMSForm pmsForm = new PMSForm();
-//        pmsForm.setVisible(true);
-//        pmsForm.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        pm.populate();
         PMSSplash dialog = new PMSSplash();
         dialog.pack();
         dialog.setVisible(true);
--- java/Projects/PMS/src/PMSForm.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class PMSForm extends JFrame{
-    private JButton generatePass;
-    private JButton addRecordButton;
-    private JButton updateRecordButton;
-    private JList passwordList;
-}
